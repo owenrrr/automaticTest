cc2833f5e13a1ac4d3fa75ebe6475c10518d15e0
owenrrr <181250092@smail.nju.edu.cn>
2020-11-18 10:57:55 +0800

    final

--- Project/Analy/src/main/java/net/mooctest/Analy.java
+++ Project/Analy/src/main/java/net/mooctest/Analy.java
@@ -20,6 +20,7 @@ import java.util.*;
  * @Description:
  */
 public class Analy {
+
     public static ClassLoader WALA_CLASS_LOADER = Analy.class.getClassLoader();
     public static final String EXCLUSION_FILE = "exclusion.txt";
 
@@ -28,9 +29,6 @@ public class Analy {
             String command = args[0];
             String target = args[1];
             String changeInfo = args[2];
-//            String command = "-c";
-//            String target = "C:\\automaticTest\\target";
-//            String changeInfo = "C:\\automaticTest\\data\\change_info.txt";
 
             if (!command.equals("-c") && !command.equals("-m")){
                 System.out.println("Command not found.");
@@ -39,11 +37,11 @@ public class Analy {
                     target = target.substring(0,target.length()-1);
                 }
                 createMethodDotFile(target);
-                createClassDotFile(target);
+                createClassDotFile();
                 if (command.equals("-c")){
-                    createSelectClass(target,changeInfo);
+                    createSelectClass(changeInfo);
                 }else{
-                    createSelectMethod(target,changeInfo);
+                    createSelectMethod(changeInfo);
                 }
             }
         }catch(Exception e){
@@ -51,22 +49,27 @@ public class Analy {
         }
     }
 
-    public static void createSelectClass(String target,String path){
+    /**
+     * @discription 创建selection-class.txt
+     * @param path:change_info.txt的路径
+     * */
+    public static void createSelectClass(String path){
 
         try{
             Map<String, HashSet<String>> changeInfo = readChangeInfo(path);
-            String str, line, caller_nofilter, callee_nofilter, caller_class, callee_class;
-            String[] tmp;
             HashSet<String> classSet = new HashSet<>();
+            String line, caller_nofilter, callee_nofilter, caller_class, callee_class;
+            String[] tmp;
+            int count;
+
+            // 将change_info.txt中改变的类加入classSet中
             for (Map.Entry<String, HashSet<String>> entry : changeInfo.entrySet()) {
                 classSet.add(entry.getKey());
             }
-            int count;
             // 读取class-cfa.dot
-//            BufferedReader classReader = new BufferedReader(new FileReader(target.substring(0,target.length()-7) + "\\data\\class-cfa.dot"));
             BufferedReader classReader = new BufferedReader(new FileReader("class-cfa.dot"));
 
-            HashSet<String> tmpSet = new HashSet<>();
+            // 将全部class-cfa.dot中涉及到的类（与改变类相关的加入到classSet中）
             do {
                 count = 0;
                 while ((line = classReader.readLine()) != null){
@@ -82,6 +85,7 @@ public class Analy {
                     callee_nofilter = tmp[1].substring(1,tmp[1].length()-1);
 
                     if (classSet.contains(caller_nofilter)){
+                        // 如果类不存在classSet中才持续循环（count != 0）
                         if (!classSet.contains(callee_nofilter)){
                             classSet.add(callee_nofilter);
                             count++;
@@ -98,6 +102,7 @@ public class Analy {
 
             HashSet<String> resultSet = new HashSet<>();
 
+            // 借由method-cfa.dot与classSet的交集得出
             while ((line = methodReader.readLine()) != null){
                 if (line.equals("digraph method {") || line.equals("}")){
                     continue;
@@ -115,6 +120,7 @@ public class Analy {
                 callee_class = "L" + tmp[0] + "/" + tmp[1] + "/" + tmp[2];
 
                 if (classSet.contains(caller_class)){
+                    // 将不是测试类或<init>()方法排除
                     if (callee_nofilter.contains("Test") && !callee_nofilter.contains("<init>()")){
                         resultSet.add(callee_nofilter);
                     }
@@ -137,14 +143,16 @@ public class Analy {
         System.out.println("select-class.txt created!");
     }
 
-    public static void createSelectMethod(String target,String path){
-        Map<String, HashSet<String>> hashMap = new HashMap<>();
-        HashSet<String> set = new HashSet<>();
-        String line, caller_class, callee_class, caller_nofilter, callee_nofilter, str;
+    /**
+     * @discription 创建selection-method.txt
+     * @param path:change_info.txt的路径
+     * */
+    public static void createSelectMethod(String path){
+        String line, str, caller_nofilter, callee_nofilter;
         String[] tmp;
 
         Map<String, HashSet<String>> changeInfo = readChangeInfo(path);
-        List<String> removeList = new ArrayList<>();   //y用于最后排除掉初始的changeInfo
+        List<String> removeList = new ArrayList<>();   //用于最后排除掉初始的changeInfo
         List<String> classList = new ArrayList<>();
         HashSet<String> methodSet = new HashSet<>();
 
@@ -157,37 +165,36 @@ public class Analy {
                     methodSet.add(str);
                 }
             }
-            // 读取method-CMD-cfa-test.dot
+            // 读取method-cfa.dot
             BufferedReader reader;
 
-            // 写select-class-test.txt
+            // 写select-class.txt
             FileWriter writer = new FileWriter(new File("selection-method.txt"));
             BufferedWriter bufferedWriter = new BufferedWriter(writer);
             HashSet<String> tmpSet = new HashSet<>();
             String next;
             int counter;  // 用于计算本次循环是否还有关联其他方法，若无则在下次循环中跳出
 
+            // 循环读取method-cfa.dot,用于找出所有与change_info.txt相关联的方法
             do {
                 counter = 0;
                 tmpSet = new HashSet<>();
                 reader = new BufferedReader(new FileReader("method-cfa.dot"));
+                // 完整读完一次method-cfa.dot
                 while ((line = reader.readLine()) != null) {
                     if (line.equals("digraph method {") || line.equals("}")){
                         continue;
                     }
+                    // 去除首尾 \t ;
                     line = line.substring(1,line.length()-1);
                     tmp = line.split(" -> ");
 
                     assert tmp.length == 2;
 
+                    // 去除首尾""
                     caller_nofilter = tmp[0].substring(1,tmp[0].length()-1);
                     callee_nofilter = tmp[1].substring(1,tmp[1].length()-1);
-                    tmp = caller_nofilter.split("\\.");  //  0:net ;1:mooctest; 2:类名
-                    caller_class = "L" + tmp[0] + "/" + tmp[1] + "/" + tmp[2];
-                    tmp = callee_nofilter.split("\\.");
-                    callee_class = "L" + tmp[0] + "/" + tmp[1] + "/" + tmp[2];
 
-//                    if (methodSet.contains(caller_nofilter) && !callee_class.equals(caller_class)){
                     if (methodSet.contains(caller_nofilter)){
                         tmpSet.add(callee_nofilter);
                     }
@@ -209,15 +216,18 @@ public class Analy {
                 assert methodSet.remove(removeList.get(i));
             }
 
+            // 最终输出，与change_info.txt相关联的类也不必输出
             for (int i=0; i<removeList.size(); i++){
                 str = removeList.get(i);
                 tmp = str.split("\\.");
                 classList.add("L"+tmp[0]+"/"+tmp[1]+"/"+tmp[2]);
             }
+
             Iterator iterator = methodSet.iterator();
             while (iterator.hasNext()){
                 next = (String) iterator.next();
                 tmp = next.split("\\.");
+                // 不是测试类，或者方法为<init>()剔除
                 if (tmp[2].contains("Test") && !next.contains("<init>()")){
                     str = "L" + tmp[0] + "/" + tmp[1] + "/" + tmp[2];
                     if (!classList.contains(str)){
@@ -233,6 +243,10 @@ public class Analy {
         System.out.println("select-method.txt created!");
     }
 
+    /**
+     * @param target
+     * @discription 创建method-cfa.dot
+     * */
     public static void createMethodDotFile(String target) throws Exception{
 
         AnalysisScope scope = AnalysisScopeReader.readJavaScope("scope.txt", new File(EXCLUSION_FILE), WALA_CLASS_LOADER);
@@ -241,6 +255,7 @@ public class Analy {
         File[] test_classes = new File(target+"\\test-classes\\net\\mooctest").listFiles();
 
         for (int i=0; i<classes.length; i++){
+            // 提出分析类本身，代表可将此分析类加入实际项目中而不影响分析
             if (!classes[i].toString().substring(classes[i].toString().length()-11).equals("Analy.class")){
                 System.out.println(classes[i].toString());
                 scope.addClassFileToScope(ClassLoaderReference.Application, new File(classes[i].toString()));
@@ -260,16 +275,18 @@ public class Analy {
         cg.init(eps);
         System.out.println("---CHACallGraph Completed---");
 
-//        File file = new File(target.substring(0,target.length()-7) + "\\data\\method-cfa.dot");
+        // 创建method-cfa.dot
         File file = new File("method-cfa.dot");
         file.createNewFile();
+
+        // 写method-cfa.dot
         FileWriter writer = new FileWriter(file);
         BufferedWriter bufferedWriter = new BufferedWriter(writer);
         bufferedWriter.write("digraph method {\r\n");
 
         // 4.遍历cg中所有的节点
         for (CGNode node : cg) {
-            // node中包含了很多信息，包括类加载器、方法信息等，这里只筛选出需要的信息
+            // 对caller和callee皆需判断是否为ShrikeBTMethod和"Application"
             if (node.getMethod() instanceof ShrikeBTMethod) {
                 if ("Application".equals(node.getMethod().getDeclaringClass().getClassLoader().toString())){
                     Iterator<CGNode> iterator = cg.getPredNodes(node);
@@ -277,6 +294,7 @@ public class Analy {
                         CGNode tmp = iterator.next();
                         if (tmp.getMethod() instanceof ShrikeBTMethod){
                             if ("Application".equals(tmp.getMethod().getDeclaringClass().getClassLoader().toString())){
+                                // 判断是否是net.mooctest包下的方法，若不是则剔除
                                 if (isNetMooctest(tmp.getMethod().getSignature()) && isNetMooctest(node.getMethod().getSignature())){
                                     bufferedWriter.write(String.format("\t\"%s\" -> \"%s\";\r\n",node.getMethod().getSignature(),tmp.getMethod().getSignature()));
                                 }
@@ -296,11 +314,14 @@ public class Analy {
         return;
     }
 
-    public static void createClassDotFile(String target){
-        // 通过读取method.dot文件来生成class.dot
+    /**
+     * @param
+     * @discription 创建class-cfa.dot，通过读取method.dot文件来生成class.dot
+     * */
+    public static void createClassDotFile(){
         Map<String,HashSet<String>> hashMap = new HashMap<>();
+        HashSet<String> set = new HashSet<>();
         String line, caller_class, callee_class;
-        HashSet<String> set = new HashSet<>();  //hashset不会重复
         String[] tmp;
         try {
             BufferedReader reader = new BufferedReader(new FileReader("method-cfa.dot"));
@@ -317,12 +338,13 @@ public class Analy {
                 //去除首尾""
                 caller_class = tmp[0].substring(1,tmp[0].length()-1);
                 callee_class = tmp[1].substring(1,tmp[1].length()-1);
-                tmp = caller_class.split("\\.");  //  0:net ;1:mooctest; 2:类名
-                caller_class = "L" + tmp[0] + "/" + tmp[1] + "/" + tmp[2];
+                tmp = caller_class.split("\\.");
+                caller_class = "L" + tmp[0] + "/" + tmp[1] + "/" + tmp[2];        // 其中索引0：net 索引1：mooctest 索引2：类名
                 tmp = callee_class.split("\\.");
                 callee_class = "L" + tmp[0] + "/" + tmp[1] + "/" + tmp[2];
 
                 if (hashMap.get(caller_class) != null){
+                    // 若已经在hashMap中，则拿出value代表的Set,加在尾部再存放
                     set = hashMap.get(caller_class);
                     set.add(callee_class);
                     hashMap.put(caller_class,set);
@@ -338,6 +360,7 @@ public class Analy {
         }
 
         try{
+            // 写class-cfa.dot
             FileWriter writer = new FileWriter(new File("class-cfa.dot"));
             BufferedWriter bufferedWriter = new BufferedWriter(writer);
             bufferedWriter.write("digraph class {\r\n");
@@ -362,6 +385,9 @@ public class Analy {
         System.out.println("class-cfa.dot created!");
     }
 
+    /**
+     * @discription 判断该类是否为net.mooctest包下的类
+     * */
     public static boolean isNetMooctest(String string){
         try{
             if (string.substring(0,12).equals("net.mooctest")){
@@ -374,6 +400,10 @@ public class Analy {
         return false;
     }
 
+    /**
+     * @discription 读取change_info.txt内容
+     * @return 返回类型Map,其中key代表caller, value代表a set of callees
+     * */
     public static Map<String, HashSet<String>> readChangeInfo(String changeInfo) {
         Map<String, HashSet<String>> hashMap = new HashMap<String, HashSet<String>>();
         HashSet<String> set = new HashSet<>();
